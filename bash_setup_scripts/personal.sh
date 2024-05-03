ba91b4dbb875b4ff0b8cb2f0da3ddd09feda0f60
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-03 13:06:40 +0200

    started fail logging

--- bash_setup_scripts/personal.sh
+++ bash_setup_scripts/personal.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+
 echo "starting dev setup: work"
 
-# sudo apt update -y
-# sudo apt upgrade -y
+FAILED_PACKAGES=""
+
+handle_fail() {
+    FAILED_PACKAGES="$FAILED_PACKAGES $1"
+    echo "Installation of $1 failed"
+}
+
+
 
 cd tasks
 chmod +x git-personal-setup.sh core-setup.sh node-setup.sh zsh-setup.sh dotfiles.sh productivity-tools.sh software.sh
@@ -17,5 +24,12 @@ chmod +x git-personal-setup.sh core-setup.sh node-setup.sh zsh-setup.sh dotfiles
 # ./software.sh
 # ./browser.sh
 # ./personal-software.sh
+
+
+if [ -n "$FAILED_PACKAGES" ]; then
+    echo "Summary: Failed to install the following packages:$FAILED_PACKAGES"
+else
+    echo "Summary: All packages installed successfully"
+fi
 
 echo "finished dev setup: work"
--- bash_setup_scripts/tasks/core-setup.sh
+++ bash_setup_scripts/tasks/core-setup.sh
@@ -3,12 +3,21 @@
 echo "starting task: core-setup"
 
 # Install System Packages 1
-sudo apt install -y build-essential cmake pkg-config libpthread-stubs0-dev
+sudo apt install -q -y build-essential || handle_fail "build-essential" 
+sudo apt install -q -y cmake || handle_fail "cmake" 
+sudo apt install -q -y pkg-config || handle_fail "pkg-config" 
+sudo apt install -q -y libpthread-stubs0-dev || handle_fail "libpthread-stubs0-dev" 
 
 # Install System Packages 2
-sudo apt install -y unzip libtool libtool-bin
+sudo apt install -q -y unzip || handle_fail "unzip"
+sudo apt install -q -y libtool || handle_fail "libtool"
+sudo apt install -q -y libtool-bin || handle_fail "libtool-bin"
 
 # Install System Packages 3
-sudo apt install -y htop lsof python3-pip moreutils
+sudo apt install -q -y htop || handle_fail "htop"
+sudo apt install -q -y lsof || handle_fail "lsof"
+sudo apt install -q -y python3-pip || handle_fail "python3-pip"
+sudo apt install -q -y moreutils || handle_fail "moreutils"
+
 
 echo "finished task: core-setup"
