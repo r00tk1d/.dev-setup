5ecc2ecf5b91ea15f8365c792f87003bead681f1
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-05 22:33:07 +0200

    fix spotify

--- bash_setup_scripts/tasks/spotify.sh
+++ bash_setup_scripts/tasks/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
 
-sudo apt-get install spotify-client
+sudo apt update
+sudo apt -qq install -y spotify-client || handle_fail "spotify-client"
