291338ef28abde37d75a20068b1d24f2186d65f0
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-07 22:27:58 +0200

    info

--- setup_scripts/tasks/stow-dotfiles.sh
+++ setup_scripts/tasks/stow-dotfiles.sh
@@ -6,6 +6,7 @@ cd "$HOME/dev_setup/dotfiles"
 
 if [[ `git status --porcelain` ]]; then
     handle_fail "stow-uncommited-changes-in-repo"
+    echo "stow aborted. Uncommited changes in repo. Please commit or stash changes and try again!"
 else
     # hack for overwrite existing dotfiles
     stow -v -t "$HOME" * --adopt
