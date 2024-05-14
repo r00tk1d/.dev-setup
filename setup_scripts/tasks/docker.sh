53bf26da47dff84eec3b55a64c2119ad9481054a
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-14 23:24:39 +0200

    improve docker install

--- setup_scripts/tasks/docker.sh
+++ setup_scripts/tasks/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get install ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,6 +13,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
+sudo apt-get -q install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
