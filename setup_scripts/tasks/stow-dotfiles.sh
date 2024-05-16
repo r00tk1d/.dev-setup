96532a3e8a33453e9943935a8aae64e74f5a08cd
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-16 20:54:02 +0200

    stow even more hacky

--- setup_scripts/tasks/stow-dotfiles.sh
+++ setup_scripts/tasks/stow-dotfiles.sh
@@ -4,13 +4,10 @@ sudo apt-get -q install -y stow || handle_fail "stow"
 
 cd "$HOME/dev_setup/dotfiles"
 
-if [[ `git status --porcelain` ]]; then
-    handle_fail "stow-uncommited-changes-in-repo"
-    echo "stow aborted. Uncommited changes in repo. Please commit or stash changes and try again!"
-else
-    # hack for overwrite existing dotfiles
-    stow -v -t "$HOME" * --adopt
-    git reset --hard
-fi
+# hack for overwrite existing dotfiles without loosing uncommited changes
+git stash
+stow -v -t "$HOME" * --adopt
+git reset --hard
+git stash pop
 
 cd "$HOME/dev_setup/setup_scripts/tasks"
