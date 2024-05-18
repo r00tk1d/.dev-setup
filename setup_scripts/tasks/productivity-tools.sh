386b27c89ccb544f50acb7a643a2db07c9cc118a
Michael Hirsch <25086447+r00tk1d@users.noreply.github.com>
2024-05-18 23:50:17 +0200

    Update productivity-tools.sh

--- setup_scripts/tasks/productivity-tools.sh
+++ setup_scripts/tasks/productivity-tools.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# sudo apt-get -q install -y tmux || handle_fail "tmux"
+sudo apt-get -q install -y gnome-tweak-tool || handle_fail "gnome-tweak-tool"
 sudo apt-get -q install -y fzf || handle_fail "fzf"
 sudo apt-get -q install -y xclip || handle_fail "xclip"
-# sudo apt-get -q install -y tldr || handle_fail "tldr"
 sudo apt-get -q install -y ripgrep || handle_fail "ripgrep"
 sudo apt-get -q install -y jq || handle_fail "jq"
 sudo apt-get -q install -y ranger || handle_fail "ranger"
