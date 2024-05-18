80555835762ce5da8c349e6092b9be25a7d75465
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-18 16:24:25 +0200

    intellij snap.. hurts but works

--- setup_scripts/tasks/intellij-community.sh
+++ setup_scripts/tasks/intellij-community.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ ! -d /opt/idea-IC-* ]; then
-    DOWNLOAD_LINK="https://download.jetbrains.com/idea/ideaIC-${INTELLIJ_VERSION}.tar.gz"
+# works but behaves a little weird
+# if [ ! -d /opt/idea-IC-* ]; then
+#     DOWNLOAD_LINK="https://download.jetbrains.com/idea/ideaIC-${INTELLIJ_VERSION}.tar.gz"
 
-    curl -fsSLo /tmp/intellij.tar.gz "${DOWNLOAD_LINK}"
-    sudo tar -xzf /tmp/intellij.tar.gz -C /opt
+#     curl -fsSLo /tmp/intellij.tar.gz "${DOWNLOAD_LINK}"
+#     sudo tar -xzf /tmp/intellij.tar.gz -C /opt
 
-    rm /tmp/intellij.tar.gz
+#     rm /tmp/intellij.tar.gz
 
-    sudo ln -s /opt/idea-IC-*/bin/idea.sh /usr/local/bin/intellij
-fi
+#     sudo ln -s /opt/idea-IC-*/bin/idea.sh /usr/local/bin/intellij
+# fi
+
+sudo snap install intellij-idea-community --classic
--- setup_scripts/tasks/intellij-ultimate.sh
+++ setup_scripts/tasks/intellij-ultimate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -d /opt/idea-IU-* ]; then
-    DOWNLOAD_LINK="https://download.jetbrains.com/idea/ideaIU-${INTELLIJ_VERSION}.tar.gz"
+# works but behaves a little weird
+# if [ ! -d /opt/idea-IU-* ]; then
+#     DOWNLOAD_LINK="https://download.jetbrains.com/idea/ideaIU-${INTELLIJ_VERSION}.tar.gz"
 
-    curl -fsSLo /tmp/intellij.tar.gz "${DOWNLOAD_LINK}"
-    sudo tar -xzf /tmp/intellij.tar.gz -C /opt
+#     curl -fsSLo /tmp/intellij.tar.gz "${DOWNLOAD_LINK}"
+#     sudo tar -xzf /tmp/intellij.tar.gz -C /opt
 
-    rm /tmp/intellij.tar.gz
+#     rm /tmp/intellij.tar.gz
 
-    sudo ln -s /opt/idea-IU-*/bin/idea.sh /usr/local/bin/intellij
-fi
+#     sudo ln -s /opt/idea-IU-*/bin/idea.sh /usr/local/bin/intellij
+# fi
 
+sudo snap install intellij-idea-ultimate --classic
