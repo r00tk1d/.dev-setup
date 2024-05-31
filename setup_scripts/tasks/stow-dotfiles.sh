27f6594a77c84b503c55ea0d6db8795b847c1b3e
Michael Hirsch <r00tk1d@users.noreply.github.com>
2024-05-31 23:32:00 +0200

    add restow

--- setup_scripts/tasks/stow-dotfiles.sh
+++ setup_scripts/tasks/stow-dotfiles.sh
@@ -7,12 +7,12 @@ cd "$HOME/dev_setup/dotfiles"
 if [[ `git status --porcelain` ]]; then
     # hack for overwrite existing dotfiles without loosing uncommited changes
     git stash
-    stow -v -t "$HOME" */ --no-folding --adopt
+    stow -v -t "$HOME" --restow */ --no-folding --adopt
     git reset --hard
     git stash pop
 else
     # hack for overwrite existing dotfiles
-    stow -v -t "$HOME" */ --no-folding --adopt
+    stow -v -t "$HOME" --restow */ --no-folding --adopt
     git reset --hard
 fi
 
