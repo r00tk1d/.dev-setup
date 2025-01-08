c4c66299b6add8f0f56efefd82bc61ac96b21801
Michael Hirsch <r00tk1d@users.noreply.github.com>
2025-01-08 22:48:24 +0100

    simplify and fix stow

--- setup_scripts/tasks/stow-dotfiles.sh
+++ setup_scripts/tasks/stow-dotfiles.sh
@@ -4,16 +4,11 @@ sudo apt-get -q install -y stow || handle_fail "stow"
 
 cd "$HOME/dev_setup/dotfiles"
 
-if [[ `git status --porcelain` ]]; then
-    # hack for overwrite existing dotfiles without loosing uncommited changes
-    git stash
-    stow -v -t "$HOME" --restow */ --no-folding --adopt
-    git reset --hard
-    git stash pop
-else
-    # hack for overwrite existing dotfiles
-    stow -v -t "$HOME" --restow */ --no-folding --adopt
-    git reset --hard
-fi
+# hack for overwrite existing dotfiles without loosing uncommited changes
+git stash
+stow -v -t "$HOME" */ --no-folding --adopt
+git reset --hard
+git stash pop
+
 
 cd "$HOME/dev_setup/setup_scripts/tasks"
