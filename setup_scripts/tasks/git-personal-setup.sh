a836715fc99354b1230c57313569b2037b024123
Michael Hirsch <r00tk1d@users.noreply.github.com>
2025-05-18 22:18:55 +0200

    add git alias correctly

--- setup_scripts/tasks/git-personal-setup.sh
+++ setup_scripts/tasks/git-personal-setup.sh
@@ -3,3 +3,7 @@
 git config --global user.name "$NAME"
 git config --global user.email "$PERSONAL_MAIL"
 git config --global commit.template ~/.git-commit-message-template.txt
+
+git config --global alias.uncommit "reset HEAD~1"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
--- setup_scripts/tasks/git-work-setup.sh
+++ setup_scripts/tasks/git-work-setup.sh
@@ -3,3 +3,6 @@
 git config --global user.name "$NAME"
 git config --global user.email "$WORK_MAIL"
 git config --global commit.template ~/.git-commit-message-template.txt
+
+git config --global alias.uncommit reset HEAD~1
+git config --global alias.lg git log --color --graph --pretty=format:'%Cred%h%Creset-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
