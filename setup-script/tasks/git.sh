0339e1e9c80347c62001ed7effe85939287ce46e
Michael Hirsch <Michael.Hirsch@bdr.de>
2025-09-05 14:36:43 +0200

    move mkdir for dotfile syumlionk preparations to symlink script

--- setup-script/tasks/git.sh
+++ setup-script/tasks/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir ~/.githooks
 mkdir ~/work
+mkdir ~/personal
 
 # install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
